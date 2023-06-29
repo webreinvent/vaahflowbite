bb9e307941ea42a1365a9cb9b45533c7761b895a
Pradeep <webreinvent@gmail.com>
2023-06-29 22:58:05 +0530

    added: few component

---
+++ atoms/ContentLoading.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center rounded-lg ">
+    <div class="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">loading...</div>
+  </div>
+</template>
---
+++ atoms/TopNavLink.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  link:{
+    default: '#',
+  },
+  target:{
+    default: null,
+  },
+  classes:{
+    default: 'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent',
+  }
+})
+</script>
+<template>
+  <NuxtLink :to="props.link" :target="props.target" :class="props.classes"><slot/></NuxtLink>
+</template>
