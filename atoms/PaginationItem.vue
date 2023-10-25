2a48a285f9ef7a206d19857c2e2874cb43b65586
Parth Sharma <parth-s001@webreinvent.com>
2023-10-25 16:15:19 +0530

    updated: files

--- atoms/PaginationItem.vue
+++ atoms/PaginationItem.vue
@@ -1,6 +1,8 @@
 <template>
   <li>
-    <a href="#" class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+    <a href="#" class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg
+    hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400
+    dark:hover:bg-gray-700 dark:hover:text-white">
       <slot/>
     </a>
   </li>
